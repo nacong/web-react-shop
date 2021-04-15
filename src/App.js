a5f34e21fa0c477880805e66b91026e6da78829c
nacong <nabinyang37@gmail.com>
2021-04-15 15:31:59 +0900

    third

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 /* eslint-disable */
-import { Navbar,Nav,NavDropdown,Form,Button,FormControl,Jumbotron } from 'react-bootstrap';
+import { Navbar,Nav,NavDropdown,Form,Button,FormControl,Jumbotron,Row,Col } from 'react-bootstrap';
 import React, { useState } from 'react';
 import './App.css';
 import shoesData from './data.js';
+import Detail from './Detail.js';
+import Navbars from './Navbars.js';
+
 
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -12,65 +15,35 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg" className="">
-        <Navbar.Brand href="#home">AST</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
-      <Route exact path="/">
-        <Jumbotron className="background">
-          <h1>A Simple Tag</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for calling
-            extra attention to featured content or information.
-          </p>
-          <p>
-            <Button variant="primary">Learn more</Button>
-          </p>
-        </Jumbotron>
-        <div className="container">
-          <div className="row">
-            {
-              shoes.map((a,i)=>{ // a = shoes의 데이터
-                return(
-                  <Column shoes={shoes[i]} i={i} key={i}/>
-                )
-              }) 
-            }
-          </div>
-        </div>
-      </Route>
-      <Route path="/detail">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button> 
+      <Navbars/>
+      <Switch>
+        <Route exact path="/">
+          <Jumbotron className="background">
+            <h1>A Simple Tag</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
+          <div className="container">
+            <div className="row">
+              {
+                shoes.map((a,i)=>{ // a = shoes의 데이터
+                  return(
+                    <Column shoes={shoes[i]} i={i} key={i}/>
+                  )
+                }) 
+              }
             </div>
           </div>
-        </div> 
-      </Route>
+        </Route>
+        <Route path="/detail/:id">
+          <Detail shoes={shoes}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
